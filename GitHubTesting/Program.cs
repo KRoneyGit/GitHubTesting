b52663540258c5a45c82c227e8d09d326df12ba5
DESKTOP-0APRN7M\Kevin <karoney@dmacc.edu>
2022-09-17 13:49:15 -0500

    Commit 3

--- GitHubTesting/Program.cs
+++ GitHubTesting/Program.cs
@@ -1,4 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Second line");
 Console.WriteLine("This is a test.");
